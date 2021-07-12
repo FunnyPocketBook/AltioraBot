b8a3f9ae1ae5570c07c95b391f5915226191ef69
FunnyPocketBook <ying-kai.dang@outlook.com>
2021-07-12 03:59:56 +0200

    Add caster command for boss caster

--- src/components/role.ts
+++ src/components/role.ts
@@ -2,12 +2,23 @@ import * as Discord from "discord.js";
 import * as CONST from "../util/constants.js";
 
 export async function enroll(message: Discord.Message, options: string[]): Promise<void> {
-  if (!message.member.roles.cache.has(CONST.ROLES.STAFF.TEAM_MANAGER) && !message.member.roles.cache.has(CONST.ROLES.STAFF.OPERATIONS_STAFF)) return;
+  if (
+    !message.member.roles.cache.has(CONST.ROLES.STAFF.TEAM_MANAGER) &&
+    !message.member.roles.cache.has(CONST.ROLES.STAFF.OPERATIONS_STAFF) &&
+    !message.member.roles.cache.has(CONST.ROLES.STAFF.BOSS_CASTER)
+  )
+    return;
   const [member, teamId] = await getMemberTeam(message, options);
   if (member === undefined || teamId === undefined) return;
   try {
     await member.roles.add(teamId);
-    await member.roles.add(CONST.ROLES.TEAMS.TRYOUT_ROLE);
+    if (teamId !== CONST.ROLES.STAFF.CASTER) {
+      await member.roles.add(CONST.ROLES.TEAMS.TRYOUT_ROLE);
+    } else {
+      message.channel.send(
+        `Welcome ${member.toString()} to the casters! Please put your information into the spreadsheet and check the pinned messages!\nhttps://docs.google.com/spreadsheets/d/16HxRhCSh2PCUa-4PsIdvWoe-DpAmbT_0Y3rtVVvcF5A/edit?usp=sharing`
+      );
+    }
     message.react(CONST.EMOJIS.CHECKMARK);
   } catch (e) {
     message.react(CONST.EMOJIS.X);
@@ -16,7 +27,12 @@ export async function enroll(message: Discord.Message, options: string[]): Promi
 }
 
 export async function derole(message: Discord.Message, options: string[]): Promise<void> {
-  if (!message.member.roles.cache.has(CONST.ROLES.STAFF.TEAM_MANAGER) && !message.member.roles.cache.has(CONST.ROLES.STAFF.OPERATIONS_STAFF)) return;
+  if (
+    !message.member.roles.cache.has(CONST.ROLES.STAFF.TEAM_MANAGER) &&
+    !message.member.roles.cache.has(CONST.ROLES.STAFF.OPERATIONS_STAFF) &&
+    !message.member.roles.cache.has(CONST.ROLES.STAFF.BOSS_CASTER)
+  )
+    return;
   const [member, teamId] = await getMemberTeam(message, options);
   if (member === undefined || teamId === undefined) return;
   try {
@@ -47,8 +63,10 @@ async function getMemberTeam(message: Discord.Message, options: string[]): Promi
   } else {
     member = await message.guild.members.fetch(user);
   }
-  const teamId = CONST.ROLES.TEAMS.TRYOUTS[options[1]?.toLocaleUpperCase()];
-  if (teamId === undefined) {
+  let teamId = CONST.ROLES.TEAMS.TRYOUTS[options[1]?.toLocaleUpperCase()];
+  if (options[1]?.toLocaleLowerCase() === "casters" || options[1]?.toLocaleLowerCase() === "caster") {
+    teamId = CONST.ROLES.STAFF.CASTER;
+  } else if (teamId === undefined) {
     const response = await message.reply(options[1] ? `Team ${options[1]} could not be found.` : "Please provide a team name.");
     setTimeout(async () => await response.delete(), 5000);
     return [undefined, undefined];
